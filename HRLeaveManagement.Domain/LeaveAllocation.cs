dff7a9fc5796fcdc812dcdf0e12de79cdd6877b7
congminh2812 <congminh281297@gmail.com>
2023-04-27 17:29:34 +0700

    add source

---
+++ HRLeaveManagement.Domain/LeaveAllocation.cs
@@ -0,0 +1,15 @@
+﻿using HRLeaveManagement.Domain.Common;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HRLeaveManagement.Domain
+{
+    public class LeaveAllocation : BaseEntity
+    {
+        public int NumberOfDays { get; set; }
+
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType? LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }    
+        public int Period { get; set; } 
+    }
+}
