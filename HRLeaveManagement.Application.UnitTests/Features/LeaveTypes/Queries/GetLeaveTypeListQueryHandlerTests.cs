e0c8a61cc5b0bd795c52ceeb5d5e054791dbeaf4
congminh2812 <congminh281297@gmail.com>
2023-05-09 17:09:11 +0700

    add source test

---
+++ HRLeaveManagement.Application.UnitTests/Features/LeaveTypes/Queries/GetLeaveTypeListQueryHandlerTests.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using HRLeaveManagement.Application.Contracts.Logging;
+using HRLeaveManagement.Application.Contracts.Persistence;
+using HRLeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using HRLeaveManagement.Application.MappingProfiles;
+using HRLeaveManagement.Application.UnitTests.Mocks;
+using Moq;
+using Shouldly;
+
+namespace HRLeaveManagement.Application.UnitTests.Features.LeaveTypes.Queries
+{
+    public class GetLeaveTypeListQueryHandlerTests
+    {
+        private readonly Mock<ILeaveTypeRepository> _mockRepo;
+        private readonly IMapper _mapper;
+        private readonly Mock<IAppLogger<GetLeaveTypesQueryHandler>> _logger;
+
+        public GetLeaveTypeListQueryHandlerTests()
+        {
+            _mockRepo = MockLeaveTypeRepository.GetMockLeaveTypeRepository();
+
+            var mapperConfig = new MapperConfiguration(s =>
+            {
+                s.AddProfile<LeaveTypeProfile>();
+            });
+
+            _mapper = mapperConfig.CreateMapper();
+            _logger = new Mock<IAppLogger<GetLeaveTypesQueryHandler>>();
+        }
+
+        [Fact]
+        public async Task GetLeaveTypeListTest()
+        {
+            var handler = new GetLeaveTypesQueryHandler(_mapper, _mockRepo.Object, _logger.Object);
+
+            var result = await handler.Handle(new GetLeaveTypesQuery(), CancellationToken.None);
+
+            result.ShouldBeOfType<List<LeaveTypeDto>>();
+            result.Count.ShouldBe(3);
+        }
+    }
+}
